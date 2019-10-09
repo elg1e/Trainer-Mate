ce5e451e9718f87b34bb51c233dad4cf6cb1a69a
Dominic Elgie <domelgie94@gmail.com>
2019-10-09 09:09:40 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+#TrainerMate
+
+A website where anyone can share there workout routines, exercises, meal plans and recipes.
+TrainerMate allows people across the world to find and share new ways for working out.
+
+##UX
+
+
+
+##Features
+
+###Existing Features
+
+###Features Left to Implement
+
+##Technologies Used
+
+##Testing
+
+##Deployment
+
+##Credits
--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'TrainerMate'
+app.config["MONGO_URI"] = os.getenv('mongodb+srv://root:root123@myfirstcluster-rieqe.mongodb.net/TrainerMate?retryWrites=true&w=majority')
 
+mongo = PyMongo(app)
 
-@app.route('/')
-def hello():
-    return 'Hello World ...again'
+app.route('/')
+@app.route('/category_page')
+def category_page():
+    return render_template("base.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
