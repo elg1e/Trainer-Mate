d3736454cca3b9ed366bd6d6b1a58113d70d4146
Dominic Elgie <domelgie94@gmail.com>
2019-10-10 09:18:07 +0000

    Fixed issue with app.py running

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'TrainerMate'
-app.config["MONGO_URI"] = os.getenv('mongodb+srv://root:root123@myfirstcluster-rieqe.mongodb.net/TrainerMate?retryWrites=true&w=majority')
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00t123@myfirstcluster-rieqe.mongodb.net/TrainerMate?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
-app.route('/')
-@app.route('/category_page')
-def category_page():
-    return render_template("base.html", tasks=mongo.db.tasks.find())
+@app.route('/')
+@app.route('/categories')
+def categories():
+    return render_template("base.html", categories=mongo.db.categories.find())
 
 
 if __name__ == '__main__':
